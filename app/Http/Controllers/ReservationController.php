137dc8e6cbd946c918f2836bd729ebbad0208abe
Lucas <scribelucas@gmail.com>
2018-09-12 00:04:51 +0200

    réservation et test dispo

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DateTime;
 use App\Models\Habitat;
 use App\Models\Reservation;
 use App\Http\Requests\ReservationRequest;
@@ -42,14 +43,47 @@ class ReservationController extends Controller
 
     /**
      * Auteur : Lucas
-     * Enregistre la réservation
+     * Enregistre la réservation en testant sa disponibilité
      */
     public function create(Habitat $habitat, ReservationRequest $request) {
         $id_locataire = Auth()->user()->id;
         $id_habitat = $habitat->id;
 
+        // montant calculé en fonction du nb de personne par nuit
         $montant = $request->nb_personne * $habitat->prix;
 
+        // récupère toutes les réservations concernant cet habitat
+        $reservations = Reservation::where('id_habitat', $id_habitat)->get();
+
+        // conversion des dates en int pour les comparer
+        $date_debut = new DateTime($request->date_debut);
+        $date_debut = $date_debut->format('Ymd');
+
+        $date_fin = new DateTime($request->date_fin);
+        $date_fin = $date_fin->format('Ymd');
+
+        // teste par rapport à chaque réservation existante si les dates dont disponibles  
+        foreach ($reservations as $reservation) {
+            // conversion des dates des réservations
+            $date_resa_debut = new DateTime($reservation->date_debut);
+            $date_resa_debut = $date_resa_debut->format('Ymd');
+
+            $date_resa_fin = new DateTime($reservation->date_fin);
+            $date_resa_fin = $date_resa_fin->format('Ymd');
+
+            // la date de début ne doit pas être dans un intervalle de date d'une des réservations 
+            if ($date_debut >= $date_resa_debut && $date_debut < $date_resa_fin) {
+
+                return redirect('habitats/' . $habitat->id)->with(['ok' => __("Désolé, ces dates ne sont pas disponibles !")]);  
+            
+            // la date de fin ne doit pas être dans un intervalle de date d'une des réservations    
+            }elseif ($date_fin > $date_resa_debut && $date_fin <= $date_resa_fin) {
+
+                return redirect('habitats/' . $habitat->id)->with(['ok' => __("Désolé, ces dates ne sont pas disponibles !")]);
+            }
+        }
+
+        // tous les cas de non disponibilité étant passés, la réservation est créé
         Reservation::create([
             'id_locataire' => $id_locataire,
             'id_habitat' => $id_habitat,
