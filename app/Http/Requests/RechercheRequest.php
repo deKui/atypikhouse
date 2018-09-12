1589a4e43f4b99273a9415ef2456201cf62cf2f5
Lucas <scribelucas@gmail.com>
2018-09-12 10:48:46 +0200

    liaison recherche

---
+++ app/Http/Requests/RechercheRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RechercheRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'destination' => 'required|string|max:255',
+            'voyageurs' => 'required|integer|max:50',
+            'depart' => 'required|date_format:Y-m-d',
+            'retour' => 'required|date_format:Y-m-d',
+        ];
+    }
+}
--- resources/views/recherche/recherche.blade.php
+++ resources/views/recherche/recherche.blade.php
@@ -2,4 +2,37 @@
 
 @section('content')
 
+<div class="container">
+
+    <div class="row">
+
+            @foreach($habitats as $habitat)
+
+            <div class="col-md-4">
+                <div class="card">
+
+                    <img class="card-img-top" src="{{ asset('storage/' . $habitat->photo) }}">
+
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Titre : {{ $habitat->titre }} </li>
+                        <li class="list-group-item">Description : {{ $habitat->description }} </li>
+                        <li class="list-group-item">Propriétaire : {{ $habitat->proprio->pseudo }} </li>
+                        <li class="list-group-item">Type : {{ $habitat->type->nom }} </li>
+                        <li class="list-group-item">Adresse : {{ $habitat->adresse }} {{ $habitat->code_postal }} {{ $habitat->ville }} </li>
+                        <li class="list-group-item">Lit(s) simple(s) : {{ $habitat->nb_lit_simple }} </li>
+                        <li class="list-group-item">Lit(s) double(s) : {{ $habitat->nb_lit_double }} </li>
+                        <li class="list-group-item">Prévu pour {{ $habitat->nb_personne_max }} personnes maximum </li>
+                        <li class="list-group-item">Disponibilité : Du {{ $habitat->date_debut_dispo }} au {{ $habitat->date_fin_dispo }} </li>
+                        <li class="list-group-item"> <a href="{{ route('habitat.show', $habitat->id) }}" class="btn btn-primary">Voir</a> </li>
+                    </ul> 
+                </div>
+            </div> 
+
+            @endforeach
+
+    </div>
+
+</div>
+
+
 @endsection
