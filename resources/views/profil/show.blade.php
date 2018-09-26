40b9a3e68320127b6dc5e65751c6734031d02117
Lucas <scribelucas@gmail.com>
2018-09-26 02:08:29 +0200

    maj noter

--- resources/views/profil/show.blade.php
+++ resources/views/profil/show.blade.php
@@ -11,16 +11,18 @@
 					<div class="card-body">
 						<ul class="list-group list-group-flush">
 						    <li class="list-group-item">Prenom : {{ $users->prenom }}</li>
-						    <li class="list-group-item">Mail : {{ $users->email }}</li>
+						    <li class="list-group-item">note : {{ $users->note }} / 5</li>
+						    
 
 						    @if ($reservations->isNotEmpty())
 						    <li class="list-group-item">
 						    	<a class="btn btn-primary atypikbutton" href="{{ route('messages.show', $users) }}"> Envoyer un message </a>
 						    </li>
-						    @endif
+						    
 						    <li class="list-group-item">
 						    	<a class="btn btn-primary atypikbutton" href="{{ route('profil.noter', $users->id) }}"> Noter </a>
 						    </li>
+						    @endif
 						<!-- VAL - L'utilisateur authentifié ne peux pas signaler son profil -->
 						@if ((auth()->user()->id) !== $users->id)
 						    <li class="list-group-item"><a href="{{ route('profil.signaleUtil', $users->id) }}" class="btn btn-primary atypikbutton">Signaler</a></li>
